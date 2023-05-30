837356bdb8d6417a381aac2da47a274c142c918a
Alikhan <alikhan.toleubay@gmail.com>
2023-05-30 08:38:30 +0600

    Dictionary (Iteration 1)
        * Added dictionary of the characters and represented morse code.
        * New Dictionary Struct
        - [x] Tables
            - [x] Alphabet
            - [x] Digits

---
+++ internal/pkg/dictionary/dictionary.go
@@ -0,0 +1,19 @@
+package dictionary
+
+/*
+* NOTE: P35S Represents - Punctuation Marks And Miscellaneous Signs
+*/
+
+type Dictionary struct {
+    All map[rune]string
+    Alphabet map[rune]string
+    Digits map[rune]string
+    P35S map[rune]string
+}
+
+func New() Dictionary {
+    return Dictionary{
+        Alphabet: *newAlphabet(),
+        Digits: *newDigits(),
+    }
+}
